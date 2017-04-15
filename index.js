f1a469fbd58f9effab85c08c8de13459491398cd
oknoorap <oknoorap@gmail.com>
2017-04-15 16:32:42 +0700

    fix: proxy handler

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ module.exports = (filename, prettify = true, indent = 2) => {
         }
       }
 
-      return new Proxy({}, validator)
+      return new Proxy(jsonFile, validator)
     }
 
     return proxyWatcher(jsonFile)
